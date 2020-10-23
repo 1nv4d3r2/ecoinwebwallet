59cd0c4b593f923f8b9f214deb653227900c40be
1nv4d3r2 <cappelozzi@gmail.com>
2020-10-23 17:37:18 -0300

    primeiro commit

---
+++ settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$server_url = "/";  // ENTER WEBSITE URL ALONG WITH A TRAILING SLASH
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "92930094";
+$db_name = "wallet";
+
+$rpc_host = "127.0.0.1";
+$rpc_port = "2332";
+$rpc_user = "ecoinrpc";
+$rpc_pass = "BiH6amoURsG3N85P9pGCgqZBrgTzQtRgzUVmL4MSjTrN";
+
+$fullname = "E-coin"; //Website Title (Do Not include 'wallet')
+$short = "ECN"; //Coin Short (BTC)
+$blockchain_tx_url = "http://ecoinblockexplorer.live/tx/"; //Blockchain Url
+$support = "ecoinbr@gmail.com"; //Your support eMail
+$hide_ids = array(1); //Hide account from admin dashboard
+$donation_address = "EafxtrsseA1uRTCbHXN8roBEgBGvVVyDKj"; //Donation Address
+
+$reserve = "0"; //This fee acts as a reserve. The users balance will display as the balance in the daemon minus the reserve. We don't reccomend setting this more than the Fee the daemon charges.
+
+?>
